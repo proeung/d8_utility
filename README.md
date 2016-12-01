924bda0875f0c2bb182f6f78c78c41245f81f134
Putra Roeung <putra.roeung@gmail.com>
2016-12-01 16:41:47 -0500

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # d8_utility
 A Drupal 8 module that handles small theme implementations, custom classes, blocks, and etc.
+More updates coming soon!
---
+++ js/d8_utility.module.js
@@ -0,0 +1,8 @@
+(function ($, Drupal) {
+
+  "use strict";
+
+  Drupal.behaviors.d8Utility = {
+    attach: function (context, settings) {
+
+})(jQuery, Drupal);
